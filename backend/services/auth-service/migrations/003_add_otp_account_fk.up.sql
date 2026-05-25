3edfd18d7abed2d01f56fe8c2a02c52d0c1743ee
Parag_wsl <paraggulati015@gmail.com>
2026-05-25 14:40:40 +0000

    Refactor: Deleted all unwanted files

--- backend/services/auth-service/migrations/003_add_otp_account_fk.up.sql
+++
@@ -1,7 +0,0 @@
--- Harden OTP challenge ownership when an account link is present.
-
-USE auth_db;
-
-ALTER TABLE otp_challenges
-  ADD CONSTRAINT fk_otp_challenges_account
-    FOREIGN KEY (account_id) REFERENCES auth_accounts(account_id);
